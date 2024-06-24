8ce8d76ec1ccee64767c85978cac5ecca328f3fe
John Francis <john9francis@gmail.com>
2024-06-24 21:57:44 +0000

    Created addrinfo on port 8080

--- main.cc
+++ main.cc
@@ -15,6 +15,23 @@ int main(){
   #endif 
   #ifdef __linux__
   std::cout << "Hello Linux World" << std::endl;
+  
+  int status;
+  struct addrinfo hints;
+  struct addrinfo* servinfo;
+
+  memset(&hints, 0, sizeof hints);
+
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  if ((status = getaddrinfo(NULL, "8080", &hints, &servinfo)) != 0){
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+  
+  std::cout << "Created addrinfo" << std::endl;
   #endif
 
 
