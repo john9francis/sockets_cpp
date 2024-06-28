3ef9965b7db16aaeb377852900c473a0ab7b9c30
John Francis <john9francis@gmail.com>
2024-06-28 18:27:01 -0400

    Got the addrinfo made

--- include/win_server.hh
+++ include/win_server.hh
@@ -20,6 +20,24 @@ int server(){
 
   std::cout << "Successfully started WSA so the dll is present" << std::endl;
 
+  #define MYPORT "8080"
+
+  struct addrinfo hints, *res;
+  ZeroMemory(&hints, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
+  hints.ai_flags = AI_PASSIVE;
+
+  iResult = getaddrinfo(NULL, MYPORT, &hints, &res);
+  if (iResult != 0){
+    std::cout << "get addr info failed: " << iResult << std::endl;
+    WSACleanup();
+    return 1;
+  }
+
+  std::cout << "get addr info succeeded" << std::endl;
+
   return 0;
 }
 } // !WindowsSockets
