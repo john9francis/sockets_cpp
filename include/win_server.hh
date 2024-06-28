018348cf761e3fa1ce4c0015fceb6c74fcc9bf07
John Francis <john9francis@gmail.com>
2024-06-28 18:22:38 -0400

    started up the WSA

--- include/win_server.hh
+++ include/win_server.hh
@@ -8,6 +8,17 @@ namespace WindowsSockets {
 int server(){
   std::cout << "Hello Windows World" << std::endl;
 
+  WSADATA wsaData;
+
+  int iResult;
+
+  iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+  if (iResult != 0){
+    std::cout << "WSAStartup failed: " << iResult << std::endl;
+    return 1;
+  }
+
+  std::cout << "Successfully started WSA so the dll is present" << std::endl;
 
   return 0;
 }
