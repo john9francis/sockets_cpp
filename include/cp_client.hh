ed18f4759d8e06899f9a5976822a98db287a1fc5
John Francis <john9francis@gmail.com>
2024-06-29 22:58:05 -0400

    removing some client hints flags to see if it will work

--- include/cp_client.hh
+++ include/cp_client.hh
@@ -18,7 +18,8 @@ int client(){
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
+  // hints.ai_flags = AI_PASSIVE; // LINUX
+  // hints.ai_protocol = IPPROTO_TCP; // WINDOWS
 
   status = getaddrinfo(/*argv[1]*/ NULL, MYPORT, &hints, &res);
   if (status != 0){
